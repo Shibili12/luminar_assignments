94001e2ba4718c1e87630207726ece8ed2db0bc9
shibili12 <shibiliazwin23@gmail.com>
2023-08-11 14:47:42 +0530

    commit

--- lib/drawerexample.dart
+++ lib/drawerexample.dart
@@ -24,120 +24,123 @@ class Drawerexample extends StatelessWidget {
                 Colors.orange.shade300,
                 Colors.red,
               ])),
-          child: ListView(children: [
-            SizedBox(
-              height: 60,
-            ),
-            ListTile(
-              leading: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    "https://images.unsplash.com/photo-1531831108325-7fe9616bc780?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2VudGxlbWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60"),
+          child: Padding(
+            padding: const EdgeInsets.all(15),
+            child: ListView(children: [
+              SizedBox(
+                height: 60,
               ),
-              title: Text(
-                "Sundar Pichai",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1531831108325-7fe9616bc780?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2VudGxlbWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=700&q=60"),
+                ),
+                title: Text(
+                  "Sundar Pichai",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: Text(
+                  "CEO of Alphabet Inc",
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
-              subtitle: Text(
-                "CEO of Alphabet Inc",
-                style: TextStyle(color: Colors.white),
+              SizedBox(
+                height: 30,
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.dashboard,
-                color: Colors.white,
+              ListTile(
+                leading: Icon(
+                  Icons.dashboard,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Dashbord",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-              title: Text(
-                "Dashbord",
-                style: TextStyle(color: Colors.white),
+              ListTile(
+                leading: Icon(
+                  Icons.leaderboard_sharp,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Leads",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.leaderboard_sharp,
-                color: Colors.white,
+              ListTile(
+                leading: Icon(
+                  Icons.person_add,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Clients",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-              title: Text(
-                "Leads",
-                style: TextStyle(color: Colors.white),
+              ListTile(
+                leading: Icon(
+                  Icons.rocket_launch,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Project",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.person_add,
-                color: Colors.white,
+              ListTile(
+                leading: Icon(
+                  Icons.subscriptions,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Subscription",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-              title: Text(
-                "Clients",
-                style: TextStyle(color: Colors.white),
+              ListTile(
+                leading: Icon(
+                  Icons.payment,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Payment",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.rocket_launch,
-                color: Colors.white,
+              ListTile(
+                leading: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "User",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-              title: Text(
-                "Project",
-                style: TextStyle(color: Colors.white),
+              ListTile(
+                leading: Icon(
+                  Icons.library_add,
+                  color: Colors.white,
+                ),
+                title: Text(
+                  "Library",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.subscriptions,
-                color: Colors.white,
+              SizedBox(
+                height: 20,
               ),
-              title: Text(
-                "Subscription",
-                style: TextStyle(color: Colors.white),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.orange, minimumSize: Size(20, 40)),
+                onPressed: () {},
+                child: Text("Logout"),
               ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.payment,
-                color: Colors.white,
-              ),
-              title: Text(
-                "Payment",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.person,
-                color: Colors.white,
-              ),
-              title: Text(
-                "User",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.library_add,
-                color: Colors.white,
-              ),
-              title: Text(
-                "Library",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orange, minimumSize: Size(20, 40)),
-              onPressed: () {},
-              child: Text("Logout"),
-            ),
-          ]),
+            ]),
+          ),
         ),
       ),
     );
